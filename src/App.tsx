35b8b9369e8606388e7b43da0c246c520b441e5c
Danil <dinkodanil@gmail.com>
2021-03-14 09:17:59 +0300

    Create default state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+type CardListState = {
+  id: number,
+  order: number,
+  text: string
+}
 
-    </div>
-  );
+function App() {
+    const [cardList, setCardList] = React.useState<CardListState[]>([
+        {id: 1, order: 3, text: "Карточка 3"},
+        {id: 2, order: 1, text: "Карточка 1"},
+        {id: 3, order: 2, text: "Карточка 2"},
+        {id: 4, order: 4, text: "Карточка 4"},
+    ])
+
+    return (
+        <div className="App">
+          {cardList.map(card => (
+              <div className={"card"}>
+                {card.text}
+              </div>
+          ))}
+        </div>
+    );
 }
 
 export default App;
